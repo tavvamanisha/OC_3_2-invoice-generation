1b87c39d4af2bc5d92e64ab6f6c61fa54101d889
manishatavva <manisha.tavva@eagles.oc.edu>
2016-08-17 18:15:19 -0500

    sprint 1 deliverable

--- src/com/se2/invoice/generate/InvoiceList.java
+++ src/com/se2/invoice/generate/InvoiceList.java
@@ -16,6 +16,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 import javax.swing.GroupLayout;
@@ -98,54 +99,47 @@ public class InvoiceList extends JFrame {
 			ResultSet rs = pst.executeQuery();
 			String[][] obj1 = new String[10000][4];
 			int i = 0, projectno = 0;
+			
+			HashMap<String, String> hashProjectClientNumber = new HashMap<String, String>();
+			HashMap<String, String> hashProjectClientName = new HashMap<String, String>();
+			HashMap<String, String> hashProjectClientAdd1 = new HashMap<String, String>();
+			HashMap<String, String> hashProjectClientAdd2 = new HashMap<String, String>();
+			HashMap<String, String> hashProjectClientCity = new HashMap<String, String>();
+			HashMap<String, String> hashProjectClientState = new HashMap<String, String>();
+			HashMap<String, String> hashProjectClientZip = new HashMap<String, String>();
+			HashMap<String, String> hashProjectClientEmail = new HashMap<String, String>();
+			HashMap<String, String> hashProjectClientInvoiceGrouping = new HashMap<String, String>();
+			HashMap<String, String> hashProjectClientInvoiceFrequency = new HashMap<String, String>();
+			HashMap<String, String> hashProjectClientPaymentTerms = new HashMap<String, String>();
 
 			while (rs.next()) {
+				
+				int timeline=rs.getInt("TIMELINE");
+				int startDateDiff = rs.getInt("StartDateDiff");
+				float timelineCompletion = startDateDiff*100/timeline;
+				
+				hashProjectClientAdd1.put(obj1[i][0], rs.getString("Address1"));
+				hashProjectClientAdd2.put(obj1[i][0], rs.getString("Address2"));
+				hashProjectClientCity.put(obj1[i][0], rs.getString("City"));
+				hashProjectClientEmail.put(obj1[i][0], rs.getString("Email"));
+				hashProjectClientInvoiceGrouping.put(obj1[i][0], rs.getString("Invoice_Grouping"));
+				hashProjectClientName.put(obj1[i][0], rs.getString("Name"));
+				hashProjectClientNumber.put(obj1[i][0], rs.getString("Client_Number"));
+				hashProjectClientState.put(obj1[i][0], rs.getString("State"));
+				hashProjectClientZip.put(obj1[i][0], rs.getString("ZIP"));
+				hashProjectClientInvoiceFrequency.put(obj1[i][0], rs.getString("Invoice_Frequency"));
+				hashProjectClientPaymentTerms.put(obj1[i][0], rs.getString("Billing_terms"));
+				
 				if (rs.getString("InvoiceDateDiff") == null) {
-					// 1st Invoice generation.
-					obj1[i][0] = rs.getString("P.ProjectNumber");
+					obj1[i][0] = String.valueOf(rs.getInt("P.ProjectNumber"));
 					obj1[i][1] = rs.getString("P.ProjectName");
-					
-					int timeline=rs.getInt("TIMELINE");
-					int startDateDiff = rs.getInt("StartDateDiff");
-					float timelineCompletion = startDateDiff*100/timeline;
-					
 					obj1[i][2] = String.valueOf(timelineCompletion);
 					obj1[i][3] = "0";
-					
 				} else {
-					
 					int date = calendar.get(Calendar.DAY_OF_MONTH);
-					
-					if(date>= 4 && date <=10){
-						if(day==Calendar.WEDNESDAY){
-							if("Monthly-Cal".equalsIgnoreCase(rs.getString("Invoice_Frequency"))){
-								projectno = rs.getInt("ProjectNumber");
-								obj1[i][0]=String.valueOf(projectno);
-								obj1[i][1] = rs.getString("P.ProjectName");
-								int timeline=rs.getInt("TIMELINE");
-								int startDateDiff = rs.getInt("StartDateDiff");
-								float timelineCompletion = startDateDiff*100/timeline;
-								obj1[i][2] = String.valueOf(timelineCompletion);
-								PreparedStatement pst1 = conn.prepareStatement("SELECT Total_amount_due from invoice_details WHERE Project_id=?");
-								pst1.setInt(1, projectno);
-								ResultSet rs1 = pst1.executeQuery();
-								int amount=0;
-								while(rs1.next()){
-									amount = amount+rs1.getInt("Total_amount_due");
-								}
-								obj1[i][3] = String.valueOf(amount);
-							}
-						}
-					}
-					
-					if (rs.getInt("InvoiceDateDiff") == 14 && "BiWeekly".equalsIgnoreCase(rs.getString("Invoice_Frequency"))) {
-						projectno = rs.getInt("ProjectNumber");
-						obj1[i][0]=String.valueOf(projectno);
-						obj1[i][1] = rs.getString("P.ProjectName");
-						int timeline=rs.getInt("TIMELINE");
-						int startDateDiff = rs.getInt("StartDateDiff");
-						float timelineCompletion = startDateDiff*100/timeline;
-						obj1[i][2] = String.valueOf(timelineCompletion);
+					if((date>= 4 && date <=10) && (day==Calendar.WEDNESDAY) && ("Monthly-Cal".equalsIgnoreCase(rs.getString("Invoice_Frequency")))
+							|| (rs.getInt("InvoiceDateDiff") == 14 && "BiWeekly".equalsIgnoreCase(rs.getString("Invoice_Frequency")))
+							|| (rs.getInt("InvoiceDateDiff") == 28 && "Monthly".equalsIgnoreCase(rs.getString("Invoice_Frequency")))){
 						PreparedStatement pst1 = conn.prepareStatement("SELECT Total_amount_due from invoice_details WHERE Project_id=?");
 						pst1.setInt(1, projectno);
 						ResultSet rs1 = pst1.executeQuery();
@@ -153,27 +147,11 @@ public class InvoiceList extends JFrame {
 						while(rs1.next()){
 							amount = amount+rs1.getInt("Total_amount_due");
 						}
-						obj1[i][3] = String.valueOf(amount);
-						
-					}else if (rs.getInt("InvoiceDateDiff") == 28 && "Monthly".equalsIgnoreCase(rs.getString("Invoice_Frequency"))) {
-						projectno = rs.getInt("ProjectNumber");
-						obj1[i][0]=String.valueOf(projectno);
+						obj1[i][0] = String.valueOf(rs.getInt("P.ProjectNumber"));
 						obj1[i][1] = rs.getString("P.ProjectName");
-						int timeline=rs.getInt("TIMELINE");
-						int startDateDiff = rs.getInt("StartDateDiff");
-						float timelineCompletion = startDateDiff*100/timeline;
-						
 						obj1[i][2] = String.valueOf(timelineCompletion);
-						PreparedStatement pst1 = conn.prepareStatement("SELECT Total_amount_due from invoice_details WHERE Project_id=?");
-						pst1.setInt(1, projectno);
-						ResultSet rs1 = pst1.executeQuery();
-						int amount=0;
-						while(rs1.next()){
-							amount = amount+rs1.getInt("Total_amount_due");
-						}
 						obj1[i][3] = String.valueOf(amount);
-						
-					}
+					}					
 				}
 				i++;
 			}
@@ -216,15 +194,96 @@ public class InvoiceList extends JFrame {
 						JFrame parent = new JFrame();
 				        JOptionPane.showMessageDialog(parent,"Please select a row.");
 					}else{
-						String today = new SimpleDateFormat("yyyy.MM.dd").format(new java.util.Date());
 						String projectNum = (String)table.getModel().getValueAt(row, 0);
 						
-						String[] ar = new String[1];
-						ar[0]=today+"_"+projectNum+".pdf";
-						GenerateInvoice.main(ar);
-						
-						JFrame parent = new JFrame();
-				        JOptionPane.showMessageDialog(parent,"Invoice for project "+projectNum+" has been generated and saved successfully.");
+						try {
+							PreparedStatement pst = conn.prepareStatement("SELECT P.ProjectNumber, C.Invoice_Frequency, "
+									+ "MAX(T.Week_start_date), CURDATE()-T.Week_start_date AS DATEDIFF, T.InvoiceGenerated "
+									+ "FROM people_timesheet T, project P, client C WHERE T.ProjectNumber=P.ProjectNumber "
+									+ "AND P.Client=C.Client_Number AND P.ProjectNumber=? AND T.People_ts_status=?");
+							pst.setString(1, projectNum);
+							pst.setString(2, "Approved");
+							ResultSet rs = pst.executeQuery();
+							
+							if(rs.next()){
+								
+								if(("Weekly".equalsIgnoreCase(rs.getString("Invoice_Frequency")) && (rs.getInt("DATEDIFF")>11 || rs.getBoolean("InvoiceGenerated")==false))
+										||("BiWeekly".equalsIgnoreCase(rs.getString("Invoice_Frequency")) && (rs.getInt("DATEDIFF")>18 || rs.getBoolean("InvoiceGenerated")==false))
+										||("Monthly".equalsIgnoreCase(rs.getString("Invoice_Frequency")) && (rs.getInt("DATEDIFF")>32 || rs.getBoolean("InvoiceGenerated")==false))
+										||("Monthly-Cal".equalsIgnoreCase(rs.getString("Invoice_Frequency")) && (rs.getInt("DATEDIFF")>42 || rs.getBoolean("InvoiceGenerated")==false))){
+									
+									String[] ar = new String[14];
+									ar[0]=projectNum;
+									ar[2]=hashProjectClientAdd1.get(projectNum);
+									ar[3]=hashProjectClientAdd2.get(projectNum);
+									ar[4]=hashProjectClientCity.get(projectNum);
+									ar[11]=hashProjectClientEmail.get(projectNum);
+									ar[10]=hashProjectClientInvoiceGrouping.get(projectNum);
+									ar[1]=hashProjectClientName.get(projectNum);
+									ar[7]=hashProjectClientNumber.get(projectNum);
+									ar[5]=hashProjectClientState.get(projectNum);
+									ar[6]=hashProjectClientZip.get(projectNum);
+									ar[9]=hashProjectClientInvoiceFrequency.get(projectNum);
+									ar[8]=hashProjectClientPaymentTerms.get(projectNum);
+									ar[12]=(String)table.getModel().getValueAt(row, 1);
+									ar[13]=new SimpleDateFormat("dd/MM/yyyy").format(new java.util.Date());
+									
+									String argument[]=new String[16];
+									argument[0]=args[0];
+									argument[1]=args[1];
+									for(int i=2;i<16;i++){
+										argument[i]=ar[i-2];
+									}
+									
+									GenerateInvoice.main(argument);
+									
+									JFrame parent = new JFrame();
+							        JOptionPane.showMessageDialog(parent,"Invoice for project "+projectNum+" has been generated and saved successfully.");
+							        
+							        dispose();
+									
+								} else {
+									JFrame parent = new JFrame();
+							        JOptionPane.showMessageDialog(parent,"Invoice for project "+projectNum+" has already been generated.");
+								}
+								
+								
+							}else{
+								String[] ar = new String[14];
+								ar[0]=projectNum;
+								ar[2]=hashProjectClientAdd1.get(projectNum);
+								ar[3]=hashProjectClientAdd2.get(projectNum);
+								ar[4]=hashProjectClientCity.get(projectNum);
+								ar[11]=hashProjectClientEmail.get(projectNum);
+								ar[10]=hashProjectClientInvoiceGrouping.get(projectNum);
+								ar[1]=hashProjectClientName.get(projectNum);
+								ar[7]=hashProjectClientNumber.get(projectNum);
+								ar[5]=hashProjectClientState.get(projectNum);
+								ar[6]=hashProjectClientZip.get(projectNum);
+								ar[9]=hashProjectClientInvoiceFrequency.get(projectNum);
+								ar[8]=hashProjectClientPaymentTerms.get(projectNum);
+								ar[12]=(String)table.getModel().getValueAt(row, 1);
+								ar[13]=new SimpleDateFormat("dd/MM/yyyy").format(new java.util.Date());
+								
+								String argument[]=new String[16];
+								argument[0]=args[0];
+								argument[1]=args[1];
+								for(int i=2;i<16;i++){
+									argument[i]=ar[i-2];
+								}
+								
+								GenerateInvoice.main(argument);
+								
+								JFrame parent = new JFrame();
+						        JOptionPane.showMessageDialog(parent,"Invoice for project "+projectNum+" has been generated and saved successfully.");
+						        
+						        dispose();
+							}
+							
+						} catch (SQLException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
 						
 					}
 					
@@ -250,46 +309,6 @@ public class InvoiceList extends JFrame {
 			header[1] = "Project Name";
 			header[2] = "Timeline Completion";
 			header[3] = "Budget Completion";
-			/*header[4] = "Timeline Completion";
-			header[5] = "Budget Completion";
-			header[6] = "After Invoice Timeline";
-			header[7] = "After Invoice Budget";*/
-
-			// String[][] obj = new String[12][12];
-
-			// dataConnectionObject dco1 = new dataConnectionObject();
-			// Connection conn1 = dco1.getConnection();
-			// PreparedStatement pst1 = conn1.prepareStatement("Select * from
-			// client");
-			// ResultSet rs1 = pst1.executeQuery();
-
-			// String[][] obj11 = new String[10000][13];
-			// int i1 = 0;
-
-			// while(rs1.next()){
-			/*
-			 * obj11[i1][0]=rs1.getString("Client_Number");
-			 * obj11[i1][1]=rs1.getString("Name");
-			 * obj11[i1][2]=rs1.getString("Address1");
-			 * obj11[i1][3]=rs1.getString("Address2");
-			 * obj11[i1][4]=rs1.getString("City");
-			 * obj11[i1][5]=rs1.getString("State");
-			 * obj11[i1][6]=rs1.getString("ZIP");
-			 * obj11[i1][7]=rs1.getString("Email");
-			 * obj11[i1][8]=rs1.getString("Contact");
-			 * obj11[i1][9]=rs1.getString("Invoice_Frequency");
-			 * obj11[i1][10]=rs1.getString("Billing_terms");
-			 * obj11[i1][11]=rs1.getString("Invoice_Grouping");
-			 * obj11[i1][12]=rs1.getString("deactivated");
-			 */
-
-			/*
-			 * boolean deactive = rs.getBoolean("deactivated");
-			 * if(deactive)obj1[i][12]="Deactive"; else obj1[i][12]="Active";
-			 */
-
-			// i1++;
-			// }
 
 			String[][] obj = Arrays.copyOfRange(obj1, 0, i);
 
